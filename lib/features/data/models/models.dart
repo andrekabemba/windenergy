0e69acba3f4b2d99ea5f8b973bcc46c12da9c12f
Eric Mpapa <me@ericmpapa.com>
2025-12-08 16:24:29 +0200

    implémentation des tests de EolienneModel

--- lib/features/data/models/models.dart
+++ lib/features/data/models/models.dart
@@ -29,4 +29,47 @@ class UtilisateurModel extends Utilisateur {
   }
 }
 
-class EolienneModel extends Eolienne {}
+class EolienneModel extends Eolienne {
+  EolienneModel({
+    super.idUtilisateur,
+    super.actif,
+    super.hashSecurite,
+    super.numeroSerie,
+    super.orientation,
+    super.puissanceGenere,
+    super.tempsDerniereMesure,
+    super.vitesseRotation,
+  });
+
+  EolienneModel.fromJSON(Map<String, Object> json)
+    : super(
+        idUtilisateur: json["idUtilisateur"]! as int,
+        actif: json["actif"]! as bool,
+        hashSecurite: json["hashSecurite"]! as String,
+        numeroSerie: json["numeroSerie"]! as String,
+        orientation: json["orientation"]! as double,
+        puissanceGenere: json["puissanceGenere"]! as double,
+        tempsDerniereMesure: DateTime.fromMillisecondsSinceEpoch(
+          json["tempsDerniereMesure"]! as int,
+        ),
+        vitesseRotation: json["vitesseRotation"]! as double,
+      );
+
+  Map<String, Object> toJSON() {
+    var nouveauTemps = 0;
+    if (tempsDerniereMesure != null) {
+      nouveauTemps = tempsDerniereMesure!.millisecondsSinceEpoch;
+    }
+
+    return <String, Object>{
+      "idUtilisateur": idUtilisateur,
+      "actif": actif,
+      "hashSecurite": hashSecurite,
+      "numeroSerie": numeroSerie,
+      "orientation": orientation,
+      "puissanceGenere": puissanceGenere,
+      "tempsDerniereMesure": nouveauTemps,
+      "vitesseRotation":vitesseRotation
+    };
+  }
+}
--- test/features/data/models/models_test.dart
+++ test/features/data/models/models_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:windenergy/features/data/models/models.dart';
-import 'package:windenergy/features/domain/entities/entities.dart';
 
 void main() {
   group("test de conversion pour [UtilisateurModel]", () {
@@ -46,6 +45,69 @@ void main() {
       );
 
       final actual = utilsateur.toJSON();
+
+      expect(actual, expected);
+    });
+  });
+
+  group("test de conversion pour [EolienneModel]", () {
+    test("devrait convertir un JSON en objet [EolienneModel]", () {
+      final expected = EolienneModel(
+        idUtilisateur: 0,
+        actif: true,
+        hashSecurite: "0",
+        numeroSerie: "0",
+        orientation: 12,
+        puissanceGenere: 100,
+        tempsDerniereMesure: DateTime.fromMillisecondsSinceEpoch(0),
+        vitesseRotation: 10,
+      );
+
+      final actual = EolienneModel.fromJSON({
+        "idUtilisateur": 0,
+        "actif": true,
+        "hashSecurite": "0",
+        "numeroSerie": "0",
+        "orientation": 12.0,
+        "puissanceGenere": 100.0,
+        "tempsDerniereMesure": 0,
+        "vitesseRotation": 10.0,
+      });
+
+      expect(actual.idUtilisateur, expected.idUtilisateur);
+      expect(actual.actif, expected.actif);
+      expect(actual.hashSecurite, expected.hashSecurite);
+      expect(actual.numeroSerie, expected.numeroSerie);
+      expect(actual.orientation, expected.orientation);
+      expect(actual.puissanceGenere, expected.puissanceGenere);
+      expect(actual.tempsDerniereMesure, expected.tempsDerniereMesure);
+      expect(actual.vitesseRotation, expected.vitesseRotation);
+    });
+
+    test("devrait convertir un [Map<String,Object>] en JSON", () {
+      final expected = {
+        "idUtilisateur": 0,
+        "actif": true,
+        "hashSecurite": "0",
+        "numeroSerie": "0",
+        "orientation": 12.0,
+        "puissanceGenere": 100.0,
+        "tempsDerniereMesure": 0,
+        "vitesseRotation": 10.0,
+      };
+
+      final eolienne = EolienneModel(
+        idUtilisateur: 0,
+        actif: true,
+        hashSecurite: "0",
+        numeroSerie: "0",
+        orientation: 12,
+        puissanceGenere: 100,
+        tempsDerniereMesure: DateTime.fromMillisecondsSinceEpoch(0),
+        vitesseRotation: 10,
+      );
+
+      final actual = eolienne.toJSON();
 
       expect(actual, expected);
     });
